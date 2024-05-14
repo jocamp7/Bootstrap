fc56966b0cc6b632a90a891243acf70fcd86045a
joana <jc557a@gmail.com>
2024-05-15 00:02:15 +0200

    casi acabado

--- funciones.js
+++ funciones.js
@@ -1,17 +1,4 @@
 
-/* 
-Por cada validación que no se cumpla muestra un mensaje durante 3 segundos y que después desaparezca.
-
-Al terminar de rellenar los datos del formulario correctamente muestra un mensaje durante 3 segundos que muestre 
-“Usuario creado correctamente” y redirige a la vista Usuarios.
-
-Muestra los mensajes utilizando los alerts de Bootstrap!!!
-
-Crea la vista Usuarios la cual debe mostrar en cards de Bootstrap los usuarios guardados en localStorage con los siguientes campos:
-Nombre
-Correo
-*/
-
 const userName=document.getElementById('inputName');
 const userEmail= document.getElementById('inputEmail');
 const userPassword= document.getElementById('inputPassword');
@@ -27,69 +14,87 @@ let array=[];
 
 let patronCorreo= /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/
 
-//Checks that a password has a minimum of 6 characters, at least 1 uppercase letter, 1 lowercase letter, and 1 number with no spaces.
-
 let patronContraseña= /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/
+
+//quitar texto del form una vez este rellenado bien
+
+function clearTextFields() {
+
+    document.getElementById("myForm").reset();
+}
+
+
+//para que desaparezca la alerta
+
+function desaparecer() {
+    document.getElementById("alerta").innerHTML = ' ';
+}
 
 
 function sendInfo (event) {
 
-    event.preventDefault();
+        event.preventDefault();
 
-    let resultadoCorreo = patronCorreo.test(userEmail.value);
+        let resultadoCorreo = patronCorreo.test(userEmail.value);
 
-    let resultadoContraseña = patronContraseña.test(userPassword.value);
+        let resultadoContraseña = patronContraseña.test(userPassword.value);
 
-    //console.log(resultadoCorreo);
+        if (userName.value === '' || userEmail.value === '' || userPassword.value=== ''|| userPassword2.value==='') {
 
-    if (userName.value === '' || userEmail.value === '' || userPassword.value=== ''|| userPassword2.value==='') {
+            document.getElementById("alerta").innerHTML = '<div class="alert alert-info largo margenes" role="alert">Por favor, rellena todos los campos</div>';
+             
+            //hacer que solo este 3 segudos y desaparezca la alerta
+            let mensajeAlerta = setTimeout(desaparecer, 3000);
 
-        console.log('Por favor, rellena todos los campos, luego aqui ira el mensaje fancy');
+        } else if(resultadoCorreo===false){
 
-    } else if(resultadoCorreo===false){
+            document.getElementById("alerta").innerHTML = '<div class="alert alert-info largo margenes" role="alert">Por favor, introduce un email válido</div>';
+             
+            let mensajeAlerta = setTimeout(desaparecer, 3000);
 
-        console.log('Por favor, introduzca un email válido, luego aqui ira el mensaje fancy');
+        }else if(userPassword.value !== userPassword2.value){
 
-    }else if(userPassword.value !== userPassword2.value){
+            document.getElementById("alerta").innerHTML = '<div class="alert alert-info largo margenes" role="alert">Introduce la misma contraseña en Contraseña y en Repetir contraseña</div>';
+             
+            let mensajeAlerta = setTimeout(desaparecer, 3000);
 
-        console.log('Por favor, compruebe que introduce la contraseña bien las 2 veces, luego aqui ira el mensaje fancy');
+        } else if(resultadoContraseña===false){
 
-    } else if(resultadoContraseña===false){
+            document.getElementById("alerta").innerHTML = '<div class="alert alert-info largo margenes" role="alert">La contraseña tiene que tener mínimo 6 carácteres, una mayúscula, una minúscula, un número, y ningún espacio</div>';
+             
+            let mensajeAlerta = setTimeout(desaparecer, 3000);
+        }
+        else{
+            console.log('todo bien, metemos user al localstorage y aqui tambien ira un alert, con un enlace a usuarios');
 
-        console.log('Su contraseña no puede tener espacios, tiene que tener mínimo 6 caracteres, al menos una mayúscula, al menos una minúscula, y al menos un número');
-    }
-    else{
-        console.log('todo bien');
-    }
-      
-     
-    //     localStorage.setItem('usuario'+i,JSON.stringify({
-    //         name: userName.value,
-    //         email: userEmail.value,
-    //         password: userPassword.value,
-    //         password2:userPassword2.value
-    //     }));
+        
+            localStorage.setItem('usuario'+i,JSON.stringify({
+                name: userName.value,
+                email: userEmail.value,
+                password: userPassword.value,
+                password2:userPassword2.value
+            }));
 
-    //     array.push(JSON.parse( localStorage.getItem('usuario'+i) ) )
+            array.push(JSON.parse( localStorage.getItem('usuario'+i) ) )
 
-    // i++;
+            i++;
 
-    clearTextFields();
+            clearTextFields();
+        }
 }
 
-function clearTextFields() {
-    userName.value = '';
-    userEmail.value = '';
-    userPassword.value = '';
-    userPassword2.value = '';
-}
+/* 
+Al terminar de rellenar los datos del formulario correctamente muestra un mensaje dentro de un alert de bootstrap durante 3 segundos
+que muestre “Usuario creado correctamente” y redirige a la vista Usuarios. Imagino con un enlace!
 
-   /*
-        Esto modificar luego al final pa que salga en las cards esas:
-        const elemento = document.createElement('p')
-        document.body.appendChild(elemento)
+Crea la vista Usuarios la cual debe mostrar en cards de Bootstrap los usuarios guardados en localStorage con los siguientes campos:
+Nombre y Correo
+*/
+
+        // const elemento = document.createElement('p')
+        // document.body.appendChild(elemento)
     
-        const data = JSON.parse(localStorage.getItem('usuario'+i))
+        // const data = JSON.parse(localStorage.getItem('usuario'+i))
 
-        document.body.lastElementChild.innerText = `${data.name} ${data.email} ${data.message}`;
-    */
+        // document.body.lastElementChild.innerText = `${data.name} ${data.email} ${data.message}`;
+    
