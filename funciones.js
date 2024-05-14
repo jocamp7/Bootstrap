692f623c25973c99b531313f67b3d22a1a26b1c9
joana <jc557a@gmail.com>
2024-05-15 00:48:47 +0200

    falta lo ultimo

--- funciones.js
+++ funciones.js
@@ -39,35 +39,44 @@ function sendInfo (event) {
 
         let resultadoContraseña = patronContraseña.test(userPassword.value);
 
+
         if (userName.value === '' || userEmail.value === '' || userPassword.value=== ''|| userPassword2.value==='') {
 
-            document.getElementById("alerta").innerHTML = '<div class="alert alert-info largo margenes" role="alert">Por favor, rellena todos los campos</div>';
+            document.getElementById("alerta").innerHTML = 
+            '<div class="alert alert-info largo margenes" role="alert">Por favor, rellena todos los campos</div>';
              
             //hacer que solo este 3 segudos y desaparezca la alerta
             let mensajeAlerta = setTimeout(desaparecer, 3000);
 
         } else if(resultadoCorreo===false){
 
-            document.getElementById("alerta").innerHTML = '<div class="alert alert-info largo margenes" role="alert">Por favor, introduce un email válido</div>';
+            document.getElementById("alerta").innerHTML = 
+            '<div class="alert alert-info largo margenes" role="alert">Por favor, introduce un email válido</div>';
              
             let mensajeAlerta = setTimeout(desaparecer, 3000);
 
         }else if(userPassword.value !== userPassword2.value){
 
-            document.getElementById("alerta").innerHTML = '<div class="alert alert-info largo margenes" role="alert">Introduce la misma contraseña en Contraseña y en Repetir contraseña</div>';
+            document.getElementById("alerta").innerHTML =
+            '<div class="alert alert-info largo margenes" role="alert">Introduce la misma contraseña en Contraseña y en Repetir contraseña</div>';
              
             let mensajeAlerta = setTimeout(desaparecer, 3000);
 
         } else if(resultadoContraseña===false){
 
-            document.getElementById("alerta").innerHTML = '<div class="alert alert-info largo margenes" role="alert">La contraseña tiene que tener mínimo 6 carácteres, una mayúscula, una minúscula, un número, y ningún espacio</div>';
+            document.getElementById("alerta").innerHTML = 
+            '<div class="alert alert-info largo margenes" role="alert">La contraseña tiene que tener mínimo 6 carácteres, una mayúscula, una minúscula, un número, y ningún espacio</div>';
              
             let mensajeAlerta = setTimeout(desaparecer, 3000);
         }
         else{
-            console.log('todo bien, metemos user al localstorage y aqui tambien ira un alert, con un enlace a usuarios');
 
-        
+            document.getElementById("alerta").innerHTML = 
+            '<div class="alert alert-info largo margenes" role="alert">Usuario creado correctamente <a href="#card">USUARIOS</a></div>';
+             
+            let mensajeAlerta = setTimeout(desaparecer, 3000);
+
+
             localStorage.setItem('usuario'+i,JSON.stringify({
                 name: userName.value,
                 email: userEmail.value,
@@ -77,24 +86,28 @@ function sendInfo (event) {
 
             array.push(JSON.parse( localStorage.getItem('usuario'+i) ) )
 
+            clearTextFields();
+
+            const data = JSON.parse(localStorage.getItem('usuario'+i))
+
+            //falta que aparezcan el nombre y el email de todos los usuarios en localstorage en la card 
+
             i++;
 
-            clearTextFields();
         }
 }
 
 /* 
-Al terminar de rellenar los datos del formulario correctamente muestra un mensaje dentro de un alert de bootstrap durante 3 segundos
-que muestre “Usuario creado correctamente” y redirige a la vista Usuarios. Imagino con un enlace!
-
 Crea la vista Usuarios la cual debe mostrar en cards de Bootstrap los usuarios guardados en localStorage con los siguientes campos:
 Nombre y Correo
 */
 
-        // const elemento = document.createElement('p')
-        // document.body.appendChild(elemento)
-    
-        // const data = JSON.parse(localStorage.getItem('usuario'+i))
-
-        // document.body.lastElementChild.innerText = `${data.name} ${data.email} ${data.message}`;
+/*
+    <div class="card margenes largo">
+         <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+         </ul>
+    </div> 
+ */
     
